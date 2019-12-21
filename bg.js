7914457f9d51df25385178362348bc2464cafeda
daengkim <kyw624@gmail.com>
2019-12-21 15:10:53 +0900

    이미지 로딩 속도 개선 테스트 2

--- bg.js
+++ bg.js
@@ -3,7 +3,7 @@ const IMG_NUMBER = 6;
 function paintImage(imgNumber) {
     const img = new Image();
     img.src = `images/${imgNumber}.png`;
-    img.onload = function() {
+    window.onload = function() {
         document.body.prepend(img);
         img.classList.add("bgImage");
     }
